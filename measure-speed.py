40de60fc20a111027954e41e71754653341fab5d
Leroy van Logchem <lr.vanlogchem@gmail.com>
2023-12-14 15:44:47 +0100

    Update measure-speed.py
    Reduce system calls, spend more time in userland

--- measure-speed.py
+++ measure-speed.py
@@ -3,49 +3,47 @@ import multiprocessing
 import datetime
 import time
 import numpy as np
+import socket
 
-def compute_heavy_task(task_queue):
-    """A function that performs heavy compute and memory-intensive tasks."""
+def compute_heavy_task():
+    """A function that performs highly CPU-intensive tasks."""
     while True:
-        if not task_queue.empty():
-            task_queue.get()
-            large_array = np.random.rand(1000, 1000)
-            np.linalg.inv(large_array)  # Inverting a large matrix
-            task_queue.put(1)           # Indicate one task completed
+        large_array = np.random.rand(2000, 2000)  # Increased matrix size
+        np.linalg.inv(large_array)  # Inverting a large matrix
+        np.dot(large_array, large_array.T)  # Matrix multiplication
 
-def report_speed(task_queue, stop_event):
+def report_speed(task_counter, num_processors, stop_event):
     """Report the speed of tasks completed per second."""
-    total_tasks = 0
+    last_count = 0
     while not stop_event.is_set():
-        current_tasks = 0
-        while not task_queue.empty():
-            task_queue.get()
-            current_tasks += 1
-        total_tasks += current_tasks
-        if current_tasks > 0:
+        current_count = task_counter.value
+        tasks_completed = current_count - last_count
+        last_count = current_count
+        if tasks_completed > 0:
+            hostname = socket.gethostname()
             current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"{current_time} Cores: {num_processors} Tasks/second: {current_tasks}")
+            print(f"{current_time} {hostname} Cores: {num_processors} Tasks/second: {tasks_completed}")
         time.sleep(1)
-    print(f"Total tasks completed: {total_tasks}")
 
 if __name__ == "__main__":
     num_processors = multiprocessing.cpu_count()
 
-    task_queue = multiprocessing.Queue()
+    task_counter = multiprocessing.Value('i', 0)
     stop_event = multiprocessing.Event()
 
     # Start task processes
-    processes = [multiprocessing.Process(target=compute_heavy_task, args=(task_queue,)) for _ in range(num_processors)]
+    processes = [multiprocessing.Process(target=compute_heavy_task) for _ in range(num_processors)]
     for p in processes:
         p.start()
 
     # Start reporting speed
-    reporter = multiprocessing.Process(target=report_speed, args=(task_queue, stop_event))
+    reporter = multiprocessing.Process(target=report_speed, args=(task_counter, num_processors, stop_event))
     reporter.start()
 
     try:
         while True:
-            task_queue.put(0)  # Add dummy task to keep the queue from being empty
+            with task_counter.get_lock():
+                task_counter.value += 1
             time.sleep(0.01)  # Adjust as needed for task frequency
     except KeyboardInterrupt:
         stop_event.set()
@@ -53,4 +51,3 @@ if __name__ == "__main__":
             p.terminate()
         reporter.terminate()
         print("\nInterrupted by user")
-
